5443046bcbadb340d4773aca1f9241e9a26af2d9
Ken Muchiri <kennethmuchiri7168@gmail.com>
2025-06-10 16:29:27 +0300

    fix the loan controller class

--- controllers/loans.js
+++ controllers/loans.js
@@ -1,10 +1,35 @@
 //loan management
-
+const { initiateSTKPush } = require('../services/mpesa');
 const db = require('../config/firebase');
 const { getAuthToken } = require('../config/daraja');
 const moment = require('moment');
 
 class LoanController {
+    async applyLateFee(loanId) {
+        const loanRef = db.collection('loans').doc(loanId);
+        const loan = (await loanRef.get()).data();
+        const daysLate = moment().diff(moment(loan.dueDate), 'days');
+        const lateFee = loan.originalAmount * 0.05 * daysLate;
+
+        //calculate 1% of the original amount will change later to 1% of late fee
+        const commission = lateFee * 0.02; // 1% of 5% is 0.05 * 0.02 = 0.01 of the originalAmount per day
+
+        await loanRef.update({
+            amount: loan.originalAmount +  lateFee,
+            lateFeeApplied: true,
+            commission: commission,
+            status: 'late',
+            updatedDate: moment().toISOString()
+        })
+
+        //send commission to ken
+        if (commission > 0) {
+            await initiateSTKPush('0706219989', Math.round(commission), `commission for loan ${loanId}`);
+        }
+        return lateFee;
+    }
+
+
     async createLoan(vendorPhone, borrowerPhone, amount, dueDate) {
         const loanRef = db.collection('loans').doc();
         await loanRef.set({
@@ -20,31 +45,13 @@ class LoanController {
         return loanRef.id;
     }
 
-    async applyLateFee(loanId) {
+    async getLoan(loanId) {
         const loanRef = db.collection('loans').doc(loanId);
-        const loan = (await loanRef.get()).data();
-
-        const daysLate = moment().diff(moment(loan.dueDate), 'days');
-        // if (daysLate > 0) {
-        //     const lateFee = daysLate * 50; // Example late fee calculation
-        //     const newAmount = loan.amount + lateFee;
-
-        //     await loanRef.update({
-        //         amount: newAmount,
-        //         status: 'late',
-        //         updatedDate: moment().toISOString()
-        //     });
-
-        //     return { success: true, message: `Late fee applied. New amount is ${newAmount}` };
-        // }
-        const lateFee = loan.originalAmount * 0.05 * daysLate;
-
-        await loanRef.update({
-            amount: loan.originalAmount + lateFee,
-            lateFeeApplied: true
-        });
-
-        return lateFee;
+        const loan = await loanRef.get();
+        if (!loan.exists) {
+            throw new Error('Loan not found');
+        }
+        return { id: loan.id, ...loan.data() };
     }
 }
 
